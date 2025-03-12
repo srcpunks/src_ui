d3ecb8181901dfa69c3119109f8839298edb46ca
Xavier Saliniere <bonjour@xaviers.sh>
2025-03-12 19:18:35 -0400

    refactor(card_poc): pass card content as optional arguments, refactor style, refactor(global): move css files to new styles directory

--- src/__fixtures__/Card.fixture.tsx
+++ src/__fixtures__/Card.fixture.tsx
@@ -1,11 +1,4 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card.tsx'
+import { Card } from '@/components/ui/card.tsx'
 import { Label } from '@/components/ui/label.tsx'
 import { Input } from '@/components/ui/input.tsx'
 import {
@@ -19,39 +12,35 @@ import { Button } from '@/components/ui/button.tsx'
 
 export default function CardFixture() {
   return (
-    <Card className="w-[350px]">
-      <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form className="relative z-10">
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
+    <Card
+      title="Create project"
+      description="Deploy your new project in one-click."
+      primaryAction={<Button>Deploy</Button>}
+      secondaryAction={<Button variant="outline">Cancel</Button>}
+      className="w-[350px]"
+    >
+      <form className="relative z-10">
+        <div className="grid w-full items-center gap-4">
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="name">Name</Label>
+            <Input id="name" placeholder="Name of your project" />
           </div>
-        </form>
-      </CardContent>
-      <CardFooter
-        leftSlot={<Button variant="outline">Cancel</Button>}
-        rightSlot={<Button className="w-full">Deploy</Button>}
-      />
+          <div className="flex flex-col space-y-1.5">
+            <Label htmlFor="framework">Framework</Label>
+            <Select>
+              <SelectTrigger id="framework">
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="next">Next.js</SelectItem>
+                <SelectItem value="sveltekit">SvelteKit</SelectItem>
+                <SelectItem value="astro">Astro</SelectItem>
+                <SelectItem value="nuxt">Nuxt.js</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
+      </form>
     </Card>
   )
 }
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,104 +1,117 @@
-import * as React from 'react'
-
 import { cn } from '@/lib/utils'
+import { ReactNode } from 'react'
 
-function Card({ className, ...props }: React.ComponentProps<'div'>) {
+interface CardHeaderProps {
+  title?: ReactNode
+  description?: ReactNode
+  className?: string
+}
+
+interface CardFooterProps {
+  primaryAction?: ReactNode
+  secondaryAction?: ReactNode
+  fitPrimaryAction?: boolean
+  className?: string
+  children?: ReactNode
+}
+
+export function Card({
+  title,
+  description,
+  primaryAction,
+  secondaryAction,
+  fitPrimaryAction,
+  className,
+  children,
+}: CardHeaderProps & CardFooterProps) {
   return (
     <div
       data-slot="card"
       className={cn('text-card-foreground flex flex-col', className)}
-      {...props}
-    />
+    >
+      <CardHeader title={title} description={description} />
+
+      <div
+        data-slot="card-content"
+        className={cn(
+          'bg-card border-x px-6 pt-3 pb-2',
+          primaryAction && 'rounded-br-4xl border-b',
+        )}
+      >
+        {children}
+      </div>
+
+      <CardFooter
+        primaryAction={primaryAction}
+        secondaryAction={secondaryAction}
+        fitPrimaryAction={fitPrimaryAction}
+      />
+    </div>
   )
 }
 
-function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
+function CardHeader({ title, description, className }: CardHeaderProps) {
   return (
     <div
       data-slot="card-header"
       className={cn(
-        'bg-card flex flex-col gap-1.5 rounded-t-4xl border border-b-0 px-6 pt-6 pb-3',
+        'bg-card flex min-h-6 flex-col gap-1.5 rounded-t-4xl border border-b-0',
         className,
       )}
-      {...props}
-    />
-  )
-}
-
-function CardTitle({ className, ...props }: React.ComponentProps<'div'>) {
-  return (
-    <div
-      data-slot="card-title"
-      className={cn('leading-none font-semibold', className)}
-      {...props}
-    />
-  )
-}
-
-function CardDescription({ className, ...props }: React.ComponentProps<'div'>) {
-  return (
-    <div
-      data-slot="card-description"
-      className={cn('text-muted-foreground text-sm', className)}
-      {...props}
-    />
-  )
-}
-
-function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
-  return (
-    <div
-      data-slot="card-content"
-      className={cn(
-        'bg-card rounded-br-4xl border border-t-0 px-6 pt-3 pb-2',
-        className,
+    >
+      {(title ?? description) && (
+        <div className="p-6 pb-3">
+          <div data-slot="card-title" className="leading-none font-semibold">
+            {title}
+          </div>
+          <div
+            data-slot="card-description"
+            className="text-muted-foreground text-sm"
+          >
+            {description}
+          </div>
+        </div>
       )}
-      {...props}
-    />
+    </div>
   )
-}
-
-interface CardFooterProps extends React.ComponentProps<'div'> {
-  leftSlot?: React.ReactNode
-  rightSlot: React.ReactNode
-  fitRight?: boolean
 }
 
 function CardFooter({
+  primaryAction,
+  secondaryAction,
+  fitPrimaryAction,
   className,
-  leftSlot,
-  rightSlot,
-  fitRight,
-  ...props
 }: CardFooterProps) {
   return (
     <div
       data-slot="card-footer"
       className={cn(
         'relative flex items-center justify-between pb-6',
+        !primaryAction && 'bg-card rounded-b-4xl border border-t-0',
         className,
       )}
-      {...props}
     >
-      <div
-        className={cn(
-          'card-footer-left flex items-center justify-center rounded-br-4xl rounded-bl-4xl px-4 py-2.5',
-          fitRight && 'flex-auto',
-        )}
-      >
-        {leftSlot}
-      </div>
+      {primaryAction && (
+        <>
+          <div
+            className={cn(
+              'card-footer-secondary',
+              fitPrimaryAction && 'flex-auto',
+            )}
+          >
+            {secondaryAction}
+          </div>
 
-      <div
-        className={cn(
-          'card-footer-right px-4 py-2.5',
-          !fitRight && 'flex-auto',
-        )}
-      >
-        {rightSlot}
-      </div>
+          <div
+            className={cn(
+              'card-footer-primary',
+              !fitPrimaryAction && 'flex-auto',
+            )}
+          >
+            {primaryAction}
+          </div>
+        </>
+      )}
     </div>
   )
 }
-
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
