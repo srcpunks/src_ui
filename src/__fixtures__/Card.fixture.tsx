9100a776a30b7218833676e6c4f235bf8452ac0f
Xavier Saliniere <bonjour@xaviers.sh>
2025-04-13 20:45:09 -0400

    style(card): finalize scifi look

--- src/__fixtures__/Card.fixture.tsx
+++ src/__fixtures__/Card.fixture.tsx
@@ -12,25 +12,31 @@ import { Button } from '@/components/ui/button.tsx'
 import { useFixtureInput, useFixtureSelect } from 'react-cosmos/client'
 
 export default function CardFixture() {
-  const [title] = useFixtureInput('Title', 'Create project')
+  const [title] = useFixtureInput('Title', 'create_project')
+  const [headerSecondary] = useFixtureInput('Header secondary', '')
+  const [headerDescription] = useFixtureInput(
+    'Header description',
+    'Deploy your new project in one-click.',
+  )
 
   const [footerLayout] = useFixtureSelect('Footer layout', {
     options: ['two-button footer', 'one-button footer', 'no footer'],
   })
 
   const twoButtonFooter = {
-    primaryAction: <Button>Deploy</Button>,
-    secondaryAction: <Button variant="outline">Cancel</Button>,
+    footerPrimary: <Button>deploy</Button>,
+    footerSecondary: <Button variant="outline">cancel</Button>,
   }
 
   const oneButtonFooter = {
-    primaryAction: <Button>Deploy</Button>,
+    footerPrimary: <Button>deploy</Button>,
   }
 
   return (
     <Card
       title={title}
-      description="Deploy your new project in one-click."
+      headerSecondary={headerSecondary}
+      description={headerDescription}
       {...(footerLayout === 'two-button footer' && twoButtonFooter)}
       {...(footerLayout === 'one-button footer' && oneButtonFooter)}
     >
@@ -38,13 +44,13 @@ export default function CardFixture() {
         <div className="grid w-full items-center gap-4">
           <div className="flex flex-col space-y-1.5">
             <Label htmlFor="name">Name</Label>
-            <Input id="name" placeholder="Name of your project" />
+            <Input id="name" placeholder="name of your project" />
           </div>
           <div className="flex flex-col space-y-1.5">
             <Label htmlFor="framework">Framework</Label>
             <Select>
               <SelectTrigger id="framework">
-                <SelectValue placeholder="Select" />
+                <SelectValue placeholder="select" />
               </SelectTrigger>
               <SelectContent position="popper">
                 <SelectItem value="next">Next.js</SelectItem>
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -3,29 +3,30 @@ import { ReactNode } from 'react'
 
 interface CardHeaderProps {
   title?: ReactNode
+  headerSecondary?: ReactNode
   description?: ReactNode
   className?: string
 }
 
 interface CartTitleProps {
   title: ReactNode
+  headerSecondary?: ReactNode
   className?: string
 }
 
 interface CardFooterProps {
-  primaryAction?: ReactNode
-  secondaryAction?: ReactNode
-  fitPrimaryAction?: boolean
+  footerPrimary?: ReactNode
+  footerSecondary?: ReactNode
   className?: string
   children?: ReactNode
 }
 
 export function Card({
   title,
+  headerSecondary,
   description,
-  primaryAction,
-  secondaryAction,
-  fitPrimaryAction,
+  footerPrimary,
+  footerSecondary,
   className,
   children,
 }: CardHeaderProps & CardFooterProps) {
@@ -34,96 +35,87 @@ export function Card({
       data-slot="card"
       className={cn('text-card-foreground flex flex-col', className)}
     >
-      <CardHeader title={title} description={description} />
+      <CardHeader
+        title={title}
+        headerSecondary={headerSecondary}
+        description={description}
+      />
 
       <div
         data-slot="card-content"
         className={cn(
-          'bg-card border-x px-6 py-2',
-          primaryAction && 'rounded-br-4xl border-b',
+          'bg-card rounded-b-4xl border-x border-b px-6 py-2',
+          footerPrimary && 'rounded-br-none',
         )}
       >
         {children}
       </div>
 
       <CardFooter
-        primaryAction={primaryAction}
-        secondaryAction={secondaryAction}
-        fitPrimaryAction={fitPrimaryAction}
+        footerPrimary={footerPrimary}
+        footerSecondary={footerSecondary}
       />
     </div>
   )
 }
 
-function CardHeader({ title, description, className }: CardHeaderProps) {
+function CardHeader({
+  title,
+  headerSecondary,
+  description,
+  className,
+}: CardHeaderProps) {
   return (
     <div
       data-slot="card-header"
       className={cn('flex min-h-6 flex-col', className)}
     >
-      {(title ?? description) && (
-        <>
-          {title && <CardTitlebar title={title} />}
-          <div
-            data-slot="card-description"
-            className={cn(
-              'bg-card text-muted-foreground rounded-tr-4xl border-x border-t px-6 pt-7 pb-4 text-sm',
-              !title && 'rounded-tl-4xl',
-            )}
-          >
-            {description}
-          </div>
-        </>
+      {title && (
+        <CardTitlebar title={title} headerSecondary={headerSecondary} />
       )}
+      <div
+        data-slot="card-description"
+        className={cn(
+          'bg-card text-muted-foreground rounded-tr-4xl border-x border-t px-6 pt-3 pb-4 text-sm',
+          !title && 'rounded-tl-4xl',
+        )}
+      >
+        <span className="relative">{description}</span>
+      </div>
     </div>
   )
 }
 
-function CardTitlebar({ title, className }: CartTitleProps) {
+function CardTitlebar({ title, headerSecondary, className }: CartTitleProps) {
   return (
     <div data-slot="card-titlebar" className={cn('card-titlebar', className)}>
       <div className={cn('card-titlebar-title flex-auto')}>{title}</div>
-      <div className={cn('card-titlebar-secondary')}></div>
+      <div className={cn('card-titlebar-secondary')}>{headerSecondary}</div>
     </div>
   )
 }
 
 function CardFooter({
-  primaryAction,
-  secondaryAction,
-  fitPrimaryAction,
+  footerPrimary,
+  footerSecondary,
   className,
 }: CardFooterProps) {
   return (
-    <div
-      data-slot="card-footer"
-      className={cn(
-        'card-footer',
-        !primaryAction && 'bg-card rounded-b-4xl border border-t-0',
-        className,
-      )}
-    >
-      {primaryAction && (
-        <>
-          <div
-            className={cn(
-              'card-footer-secondary',
-              fitPrimaryAction && 'flex-auto',
-            )}
-          >
-            {secondaryAction}
-          </div>
+    <>
+      {footerPrimary && (
+        <div
+          data-slot="card-footer"
+          className={cn(
+            'card-footer',
+            !footerPrimary && 'bg-card rounded-b-4xl border border-t-0',
+            className,
+          )}
+        >
+          <div className="card-footer-secondary">{footerSecondary}</div>
 
-          <div
-            className={cn(
-              'card-footer-primary',
-              !fitPrimaryAction && 'flex-auto',
-            )}
-          >
-            {primaryAction}
-          </div>
-        </>
+          <div className="card-footer-primary">{footerPrimary}</div>
+        </div>
       )}
-    </div>
+    </>
   )
 }
